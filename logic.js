4d55d492abb08f2c6a06b9f9db507899b0e1c232
KKoder1991 <konondlovu@icloud.com>
2021-03-30 10:39:12 +0200

    Remove extra brackets from arrays.

--- logic.js
+++ logic.js
@@ -8,22 +8,18 @@ const reviews = [
         
     },
     {
-        {
             id: 2,
             name: "Ahmed Rabbhati",
             job: "Developer",
             img: "/Users/Kono/Documents/Programming/java_script_practice/reviews-javaScript/Ahmed.jpg",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             
-        },
-    {
-        {
+    },
+     {
             id: 3,
             name: "Mikhaila Stevens",
             job: "UX Designer",
             img: "/Users/Kono/Documents/Programming/java_script_practice/reviews-javaScript/Mikhaila.jpg",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."   
         },
-    },
 ]
