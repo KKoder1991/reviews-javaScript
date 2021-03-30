6fbdabfdc0ff3d64f7810f30a91ceab3edeefdd3
KKoder1991 <konondlovu@icloud.com>
2021-03-30 11:33:16 +0200

    Changed img url path from to relative path.

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@ const reviews = [
         id: 1,
         name: "Thandazo Nobuja",
         job: "Boss Man",
-        img: "/Users/Kono/Documents/Programming/java_script_practice/reviews-javaScript/Njabulo.jpg",
+        img: "style-resources/Njabulo.jpg",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         
     },
@@ -11,7 +11,7 @@ const reviews = [
             id: 2,
             name: "Ahmed Rabbhati",
             job: "Developer",
-            img: "/Users/Kono/Documents/Programming/java_script_practice/reviews-javaScript/Ahmed.jpg",
+            img: "style-resources/Ahmed.jpg",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             
     },
@@ -19,7 +19,7 @@ const reviews = [
             id: 3,
             name: "Mikhaila Stevens",
             job: "UX Designer",
-            img: "/Users/Kono/Documents/Programming/java_script_practice/reviews-javaScript/Mikhaila.jpg",
+            img: "style-resources/Mikhaila.jpg",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."   
         },
 ]
